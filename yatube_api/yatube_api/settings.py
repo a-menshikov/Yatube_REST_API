13a352fab70886b77c9fb8d8285ba34bfcc62b60
a-menshikov <a.menshikov1989@gmail.com>
2023-01-18 12:14:26 +0300

    fix pep

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -104,8 +104,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-   'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-   'AUTH_HEADER_TYPES': ('Bearer',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'AUTH_HEADER_TYPES': ('Bearer',),
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
